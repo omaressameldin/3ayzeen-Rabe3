59e922482e9c3e58426235abdb079e2aeb14eda5
omar sharkawy <omarsharkawy94@gmail.com>
2015-11-13 20:09:19 +0200

    pulling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,29 @@
 class PostsController < ApplicationController
   def index
-    @post = Post.find :all
+    # @post = Post.find :all
   end
-  
+
   def show
     @post = Post.find params[:id]
   end
-  
+
   def new
     @post = Post.new
   end
-  
+
   def create
     @post = Post.new params[:post]
+    current_user.user_id = 1
+    @post.user_id = current_user.user_id
+    @post.receiver_id = params[:id]
+    @post.content = params[:content]
     if @post.save
       redirect_to :action => 'show', :id => @post.id
     else
       render :action => 'new'
     end
   end
-  
+
   def destroy
     @post = Post.find params[:id]
     @post.destroy
